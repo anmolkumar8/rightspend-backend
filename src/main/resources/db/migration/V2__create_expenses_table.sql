f8a93ea19d7af947d76a3f40df126b4596b4d6f0
anmolkumar8 <anmol.krhjp@gmail.com>
2026-02-01 03:23:38 +0530

    Fix schema creation using Hibernate ddl-auto

--- src/main/resources/db/migration/V2__create_expenses_table.sql
+++ src/main/resources/db/migration/V2__create_expenses_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE expenses (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    amount DOUBLE NOT NULL,
-    category VARCHAR(100),
-    date DATE NOT NULL,
-    user_id BIGINT NOT NULL,
+  id BIGSERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  amount DOUBLE PRECISION NOT NULL,
+  category VARCHAR(100),
+  date DATE NOT NULL,
+  user_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_expense_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
+  CONSTRAINT fk_expense_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
 );
